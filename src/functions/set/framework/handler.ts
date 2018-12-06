26d25bae945c8e66ba0302c06c547543d4cfb8a7
John Morley <john.morley@bjss.com>
2018-12-06 13:32:47 +0000

    Remove build process test functions

--- src/functions/set/framework/handler.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyEvent, Context, Callback } from 'aws-lambda';
-import createResponse from '../../../common/application/utils/createResponse';
-
-export function handler(event: APIGatewayProxyEvent, context: Context, callback: Callback) {
-  callback(null, createResponse({}));
-}
