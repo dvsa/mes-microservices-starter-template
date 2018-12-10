50c4ae8faec3f6146f72f23a2e12412abf1798f2
John Morley <john.morley@bjss.com>
2018-12-10 15:12:52 +0000

    Fix unit test

--- src/common/application/utils/__tests__/createResponse.spec.ts
+++ src/common/application/utils/__tests__/createResponse.spec.ts
@@ -1,9 +1,9 @@
 import createResponse from '../createResponse';
-import { IResponse } from '../../interfaces/IResponse';
+import Response from '../../../../common/application/api/Response';
 
 describe('createResponse', () => {
   it('should create a response with 200 status code when no status code is specified', () => {
-    const response: IResponse = createResponse(null);
+    const response: Response = createResponse(null);
     expect(response.statusCode).toBe(200);
   });
 });
