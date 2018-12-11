36325eaaaaa8e653ef13cba1dbd31d55f88ae425
John Morley <john.morley@bjss.com>
2018-12-11 16:17:26 +0000

    Elaborate failure case and unit tests for getJournal handler

---
+++ src/functions/getJournal/framework/__tests__/handler.spec.ts
@@ -0,0 +1,61 @@
+import context, { ServiceIdentifiers } from '../../application/context';
+import { ExaminerWorkSchedule } from '../../../../common/domain/Journal';
+import { JournalRetriever } from '../../application/service/JournalRetriever';
+import { handler } from '../handler';
+const lambdaTestUtils = require('aws-lambda-test-utils');
+import * as createResponse from '../../../../common/application/utils/createResponse';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+
+describe('getJournal handler', () => {
+  const fakeJournal: ExaminerWorkSchedule = {
+    examiner: {
+      staffNumber: '123',
+    },
+  };
+  let dummyApigwEvent: APIGatewayEvent;
+  let dummyContext: Context;
+  let createResponseSpy: jasmine.Spy;
+
+  class ErroringJournalRetriever implements JournalRetriever {
+    getJournal(): ExaminerWorkSchedule {
+      throw new Error('Journal retrieve failed');
+    }
+  }
+
+  class FakeJournalRetriever implements JournalRetriever {
+    getJournal(): ExaminerWorkSchedule {
+      return fakeJournal;
+    }
+  }
+
+  beforeEach(() => {
+    createResponseSpy = spyOn(createResponse, 'default');
+    dummyApigwEvent = lambdaTestUtils.mockEventCreator.createAPIGatewayEvent();
+    dummyContext = lambdaTestUtils.mockContextCreator(() => null);
+  });
+
+  describe('given the JournalRetriever returns a journal', () => {
+    it('should return a successful response with the journal', async () => {
+      context.rebind(ServiceIdentifiers.JournalRetriever)
+        .toConstantValue(new FakeJournalRetriever());
+      createResponseSpy.and.returnValue({ statusCode: 200 });
+
+      const resp = await handler(dummyApigwEvent, dummyContext);
+
+      expect(resp.statusCode).toBe(200);
+      expect(createResponse.default).toHaveBeenCalledWith(fakeJournal);
+    });
+  });
+
+  describe('given the JournalRetriever throws', () => {
+    it('should return an error response', async () => {
+      context.rebind(ServiceIdentifiers.JournalRetriever)
+        .toConstantValue(new ErroringJournalRetriever());
+      createResponseSpy.and.returnValue({ statusCode: 502 });
+
+      const resp = await handler(dummyApigwEvent, dummyContext);
+
+      expect(resp.statusCode).toBe(502);
+    });
+  });
+});
--- src/functions/getJournal/framework/handler.ts
+++ src/functions/getJournal/framework/handler.ts
@@ -3,11 +3,18 @@ import context, { ServiceIdentifiers } from '../application/context';
 import { JournalRetriever } from '../application/service/JournalRetriever';
 import createResponse from '../../../common/application/utils/createResponse';
 import { ExaminerWorkSchedule } from '../../../common/domain/Journal.d';
+import Response from '../../../common/application/api/Response';
 
-const journalRetriever: JournalRetriever =
-  context.get<JournalRetriever>(ServiceIdentifiers.JournalRetriever);
+export async function handler(event: APIGatewayProxyEvent, fnCtx: Context) {
+  const journalRetriever = context.get<JournalRetriever>(ServiceIdentifiers.JournalRetriever);
 
-export async function handler(event: APIGatewayProxyEvent, context: Context) {
-  const journal: ExaminerWorkSchedule = journalRetriever.getJournal();
-  return createResponse(journal);
+  let response: Response;
+  try {
+    const journal: ExaminerWorkSchedule = journalRetriever.getJournal();
+    response = createResponse(journal);
+  } catch (err) {
+    console.error(err);
+    response = createResponse('Unable to retrieve journal', 502);
+  }
+  return response;
 }
