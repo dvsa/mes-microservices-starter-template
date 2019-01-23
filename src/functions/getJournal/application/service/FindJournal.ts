e74296680df2ad0f6f2d9f071b3f85d330c5d61e
Tom Billington <t.billington@kainos.com>
2019-01-23 13:53:48 +0000

    fixed lint errors

--- src/functions/getJournal/application/service/FindJournal.ts
+++ src/functions/getJournal/application/service/FindJournal.ts
@@ -2,26 +2,25 @@ import { getJournal } from '../../framework/aws/DynamoJournalRepository';
 import { ExaminerWorkSchedule } from '../../../../common/domain/Journal';
 import * as moment from 'moment';
 
-
 /**
- *  The class below is also found in MES-Mobile-App in bin/update-mock-data.ts 
+ *  The class below is also found in MES-Mobile-App in bin/update-mock-data.ts
  *  So if you find a bug please fix it in both places.
  */
 class DateUpdater {
 
-  private dateFormat = 'YYYY-MM-DDTHH:mm:ssZ'
+  private dateFormat = 'YYYY-MM-DDTHH:mm:ssZ';
 
   constructor(public data: ExaminerWorkSchedule) { }
 
   public updateTestSlots = (): DateUpdater => {
-    if (!this.data.testSlots) { return this }
+    if (!this.data.testSlots) { return this; }
 
     this.updateSlots(this.data.testSlots);
     return this;
   }
 
   public updateNonTestActivities = (): DateUpdater => {
-    if (!this.data.nonTestActivities) { return this };
+    if (!this.data.nonTestActivities) { return this; }
 
     this.updateSlots(this.data.nonTestActivities);
     return this;
@@ -40,47 +39,49 @@ class DateUpdater {
     const newDate = this.createMoment();
     let dateProcessing = this.createMoment(slots[0].slotDetail.start);
 
-    slots.forEach(slot => {
-      if (!slot.slotDetail || !slot.slotDetail.start) { return }
+    slots.forEach((slot) => {
+      if (!slot.slotDetail || !slot.slotDetail.start) { return; }
 
-      const slotDate = this.createMoment(slot.slotDetail.start)
+      const slotDate = this.createMoment(slot.slotDetail.start);
 
       dateProcessing = this.caculateNewProcessingDate(dateProcessing, slotDate, newDate);
 
       slot.slotDetail.start = this.updateDate(slotDate, newDate);
     });
-  };
+  }
 
   private doesMatch = (a: moment.Moment, b: moment.Moment): boolean => {
     return a.isSame(b, 'day');
-  };
+  }
 
   private caculateDiffInDays = (a: moment.Moment, b: moment.Moment): number => {
     return this.createMoment(a).startOf('day').diff(this.createMoment(b).startOf('day'), 'days');
-  };
+  }
 
   private createMoment = (date?: string | moment.Moment): moment.Moment => {
     if (date) {
       return moment(date, this.dateFormat, true);
     }
-    return moment()
-  };
+    return moment();
+  }
 
   private updateDate = (currentDate: moment.Moment, newDate: moment.Moment): string => {
     const daysToAdd = this.caculateDiffInDays(newDate, currentDate);
     return currentDate.add(daysToAdd, 'days').format(this.dateFormat);
-  };
+  }
 
-  private caculateNewProcessingDate =
-    (dateProcessing: moment.Moment, itemDate: moment.Moment, newDate: moment.Moment): moment.Moment => {
-      if (this.doesMatch(dateProcessing, itemDate)) { return dateProcessing; }
+  private caculateNewProcessingDate = (
+    dateProcessing: moment.Moment,
+    itemDate: moment.Moment,
+    newDate: moment.Moment,
+  ): moment.Moment => {
+    if (this.doesMatch(dateProcessing, itemDate)) { return dateProcessing; }
 
-      const daysToAdd = this.caculateDiffInDays(itemDate, dateProcessing)
-      newDate.add(daysToAdd, 'days');
-      return this.createMoment(itemDate);
-    };
+    const daysToAdd = this.caculateDiffInDays(itemDate, dateProcessing);
+    newDate.add(daysToAdd, 'days');
+    return this.createMoment(itemDate);
+  }
 }
-
 
 export async function findJournal(staffNumber: string): Promise<ExaminerWorkSchedule | null> {
   const journalWrapper = await getJournal(staffNumber);
@@ -96,7 +97,8 @@ export async function findJournal(staffNumber: string): Promise<ExaminerWorkSche
 }
 
 /**
- * All Code below this line is a Temporary measure that will make sure slots have the correct dates. 
+ * All Code below this line is a Temporary measure that will make sure slots
+ * have the correct dates.
  */
 
 function updateDates(journal: ExaminerWorkSchedule): ExaminerWorkSchedule {
