2da995adbb6d7dc25694e01a53b876957f0e89fa
Ammar Haider <ammar.haider@bjss.com>
2019-02-18 11:09:30 +0000

    ignores packaging funcs with 'local' prefix

--- package.sh
+++ package.sh
@@ -7,10 +7,14 @@ bundle_dir="build/bundle/"
 artefact_dir="artefacts/"
 version_num=$(jq -r '.version' < package.json | cut -d . -f 1,2).$(date +%s)
 git_rev=$(git rev-parse --short HEAD)
+ignore_func_prefix="local"
 
 mkdir -p ${artefact_dir}
 functions=$(npx yaml2json serverless.yml | jq -r '.functions | keys | .[]')
 for func_name in ${functions}; do
+  if [ "$func_name"=="*$ignore_func_prefix*" ]; then
+  continue
+  fi
   if [ -z ${LAMBDAS} ] || echo ${LAMBDAS} | grep ${func_name}; then
     bundle_path="${bundle_dir}${func_name}.js"
     zip_filename="${func_name}-${version_num}-${git_rev}.zip"
@@ -21,7 +25,7 @@ for func_name in ${functions}; do
 done
 
 if [ -d coverage ]; then
-  coverage_filename="template-coverage-${version_num}-${git_rev}.zip"
+  coverage_filename="journal-coverage-${version_num}-${git_rev}.zip"
   coverage_path="${artefact_dir}${coverage_filename}"
   zip ${coverage_path} coverage
   echo "COVERAGE ARTIFACT: coverage => ${coverage_path}"
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const YAML = require('yamljs');
 
 const allEntries = Object.keys(YAML.load('serverless.yml').functions)
+  .filter(func => (func.indexOf('local') === -1))
   .reduce((entryObj, functionName) => {
     entryObj[functionName] = `.${path.sep}${path.join('src', 'functions', functionName, 'framework', 'handler.ts')}`
     return entryObj;
@@ -10,9 +11,8 @@ const allEntries = Object.keys(YAML.load('serverless.yml').functions)
 module.exports = env => ({
   target: 'node',
   mode: 'production',
-  entry: env && env.lambdas
-    ? env.lambdas.split(',').reduce((entryObj, fnName) => ({ ...entryObj, [fnName]: allEntries[fnName] }), {})
-    : allEntries,
+  entry: env && env.lambdas ?
+    env.lambdas.split(',').reduce((entryObj, fnName) => ({ ...entryObj, [fnName]: allEntries[fnName] }), {}) : allEntries,
   module: {
     rules: [
       {
